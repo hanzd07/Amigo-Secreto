c3b1e1ead28a7de1c4bd940101cd90c9df21856e
hanz diener <hanz.diener@gmail.com>
2025-02-17 21:24:48 -0500

    Agrega 2 funciones para agregar amigo y mostrar amigo
    Se agrego 2 funciones, la función agregarAmigo() se obtiene el valor del input y se elimina espacios en blanco con trim(), si el valor no esta vacío se agrega el nombre al array y se limpia el input, si el input esta vacío se mostrara una alerta. se agrega la función mostrarAmigos(), esta función va a mostrar los nombres ingresados.
    Funcion mostrarAmigos() esta función limpia el contenido de la lista, recorre el array amigos y se crea un elemento <li> por cada nombre, se añade cada <li>.

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ let amigos = [];
 let inputAmigo = document.getElementById("amigo");
 let listaAmigos = document.getElementById("listaAmigos");
 let resultado = document.getElementById("resultado");
+//Funcion para agregar amigos
+function agregarAmigo(){
+    let nuevoAmigo = inputAmigo.value.trim();
+    if(nuevoAmigo){
+        amigos.push(nuevoAmigo);
+        inputAmigo.value = "";
+        mostrarAmigos();
+    }else {
+        alert("Por favor, ingrese un nombre");
+    }
+}
+//Mostrar la lista de amigos
+function mostrarAmigos() {
+    listaAmigos.innerHTML = "";
+    amigos.forEach(function(amigo){
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
